ff8d2b366ba780e07def1416d63fc6814246426b
marcioandrade <marcioandrade.dev@gmail.com>
2021-09-09 16:54:18 -0300

    configurando o PWA

--- next.config.js
+++ next.config.js
@@ -1,3 +1,14 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const withPWA = require('next-pwa')
+const isProd = process.env.NODE_ENV === 'production'
+
+module.exports = withPWA({
+  pwa: {
+    dest: 'public',
+    disable: !isProd,
+  },
+})
--- src/components/Main/styles.ts
+++ src/components/Main/styles.ts
@@ -28,5 +28,5 @@ export const Description = styled.h2`
 
 export const Illustration = styled.img`
   margin-top: 3rem;
-  width: min-content(30rem, 100%);
+  width: min(30rem, 100%);
 `
