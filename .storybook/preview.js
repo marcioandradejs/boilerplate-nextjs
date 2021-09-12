d1b63ab29b65794a673224ac2c82d1af6a8ffae0
marcioandrade <marcioandrade.dev@gmail.com>
2021-09-12 13:50:06 -0300

    configurando storybook

---
+++ .storybook/preview.js
@@ -0,0 +1,15 @@
+import { addDecorator } from '@storybook/react'
+import withGlobalStyles from './withGlobalStyles'
+
+addDecorator(withGlobalStyles)
+
+
+// export const parameters = {
+//   actions: { argTypesRegex: "^on[A-Z].*" },
+//   controls: {
+//     matchers: {
+//       color: /(background|color)$/i,
+//       date: /Date$/,
+//     },
+//   },
+// }
