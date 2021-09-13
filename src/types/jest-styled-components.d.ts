634d2f71b383813e0f512ac43bf4b935c4fe3fa7
marcioandrade <marcioandrade.dev@gmail.com>
2021-09-13 20:29:47 -0300

    fix eslint error in jest-stuled-components.d.ts

--- src/types/jest-styled-components.d.ts
+++ src/types/jest-styled-components.d.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 /* eslint-disable @typescript-eslint/no-explicit-any */
 /* eslint-disable @typescript-eslint/ban-types */
 import { Plugin, NewPlugin } from 'pretty-format'
