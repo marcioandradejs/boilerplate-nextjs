571405d0969b5ee0689916112c3095656debc07d
marcioandrade <marcioandrade.dev@gmail.com>
2021-09-13 21:50:12 -0300

    Add next eslint plugin to existing config and others config to update all dependencies in package.json file

--- next.config.js
+++ next.config.js
@@ -1,7 +1,3 @@
-module.exports = {
-  reactStrictMode: true,
-}
-
 // eslint-disable-next-line @typescript-eslint/no-var-requires
 const withPWA = require('next-pwa')
 const isProd = process.env.NODE_ENV === 'production'
