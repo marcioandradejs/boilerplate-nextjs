3a37691b58a4ad2664183256dae8280940ebb929
marcioandrade <marcioandrade.dev@gmail.com>
2021-10-09 00:32:38 -0300

    fix of the jest test

--- jest.config.js
+++ jest.config.js
@@ -4,4 +4,5 @@ module.exports = {
   collectCoverage: true,
   collectCoverageFrom: ['src/**/*.ts(x)?', '!src/**/stories.tsx'],
   setupFilesAfterEnv: ['<rootDir>/.jest/setup.ts'],
+  modulePaths: ['<rootDir>/src/', '<rootDir>/.jest']
 }
